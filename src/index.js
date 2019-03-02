f8460641df93395206e82d7583c04054f144d94f
Justin Haupt <jhaupt100@gmail.com>
2019-03-01 21:16:35 -0800

    integrated webpack with project and switched to SASS styles

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+require('normalize.css/normalize.css');
+require('./styles/index.scss');
+
+// $(function() {
+//
+//   var openWedges = function y() {
+//
+//     var wedges = document.getElementById('wedges').getBoundingClientRect();
+//     // console.log(wedges.top);
+//
+//
+//
+//     $('.parallax').on('scroll', openWedges);
+//
+//   //window.addEventListener('scroll', openWedges);
+//   }
+//
+// });
